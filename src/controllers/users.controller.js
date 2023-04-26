b4c1ac76392e20ab890bf67ff9b5a6ba51cb4c97
Matías Aybar <matiasaybar89@gmail.com>
2023-04-26 18:20:00 -0300

    feat(task-2): User registration added

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,20 @@
+import bcryptjs from 'bcryptjs';
+import { pool } from '../db/connection.js';
+export const UserRegistration = async (req, res) => {
+    const { name, email, role, password } = req.body;
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    try {
+        const [rows] = await pool.query('INSERT INTO users (name, email, role, password) VALUES (?, ?, ?, ?)', [name, email, role, hashedPassword]);
+        const newUser = {
+            id: rows.insertId,
+            name,
+            email,
+            password: hashedPassword
+        };
+        return res.status(201).json(newUser);
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+};
