d29d99df41a8d65d844cfc14bdfb3d31072084b2
Matías Aybar <matiasaybar89@gmail.com>
2023-05-10 19:02:23 -0300

    feat(task-6): home services

--- src/controllers/warehouses.controller.js
+++ src/controllers/warehouses.controller.js
@@ -1,7 +1,7 @@
 import { pool } from '../db/connection.js';
 
 export const getWarehouses = async (req, res) => {
-    const result = await pool.query('SELECT * FROM warehouses');
+    const result = await pool.query('SELECT * FROM warehouses ORDER BY id DESC');
     res.json(result[0]);
 }
 
@@ -13,8 +13,17 @@ export const postWarehouses = async (req, res) => {
     const { name, address, code, state, country, zip } = req.body;
     const warehouse = await getWarehouse(code);
     if (warehouse) {
-        return res.status(400).json({ message: 'Warehouse exists' });
+        return res.status(400).json({ message: 'Warehouse exists', status: 400 });
     }
     const result = await pool.query('INSERT INTO warehouses (name, address, code, state, country, zip) VALUES (?, ?, ?, ?, ?, ?)', [name, address, code, state, country, zip]);
-    res.json(result[0]);
+    return res.json({
+        message: result[0],
+        status: 200,
+    });
 }
+
+export const deleteWarehouse = async (req, res) => {
+    const { id } = req.params;
+    const [result] = await pool.query('DELETE FROM warehouses WHERE code = ?', [id]);
+    return res.json(result);
+};
